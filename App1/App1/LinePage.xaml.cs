a39b74248005c1f7b02ab87869e0c2b5dcbb9f8d
Bird Lin <bird00lin@yahoo.com.tw>
2018-12-01 00:17:41 +0800

    增加蒼井空清單

---
+++ App1/App1/LinePage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LinePage : ContentPage
+    {
+        ObservableCollection<string> _friends;
+
+        public LinePage()
+        {
+            InitializeComponent();
+            binding();
+            lvFriend.ItemsSource = _friends;
+
+        }
+
+        private void binding()
+        {
+            _friends = new ObservableCollection<string>()
+            {
+                "蒼井空","456","789"
+
+            };
+        }
+
+        private async void ListView_Refreshing(object sender, EventArgs e)
+        {
+            await Task.Delay(500);
+            lvFriend.IsRefreshing = false;
+        }
+
+        private void LvFriend_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            // await
+            lvFriend.SelectedItem = null;
+
+        }
+    }
+
+    public class DoubleToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double multiplier;
+
+            if (!Double.TryParse(parameter as string, out multiplier))
+                multiplier = 1;
+
+            return (int)Math.Round(multiplier * (double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double divider;
+
+            if (!Double.TryParse(parameter as string, out divider))
+                divider = 1;
+
+            return ((double)(int)value) / divider;
+        }
+    }
+}
--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -18,5 +18,10 @@ namespace App1
         {
             await Navigation.PushAsync(new CalculatorPage());
         }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LinePage());
+        }
     }
 }
